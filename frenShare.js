17b15e9eb12596037ce2cc496c66f20c223660c5
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2023-01-25 23:27:32 -0500

    Update frenShare.js

--- frenShare.js
+++ frenShare.js
@@ -31,28 +31,29 @@ async function init(networks,rpc_,command,command_b) {
     let iSplitter__ABI = [ { "inputs": [], "stateMutability": "payable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "token", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "wad", "type": "uint256" } ], "name": "WithdrawToken", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "uint256", "name": "wad", "type": "uint256" } ], "name": "Withdrawal", "type": "event" }, { "stateMutability": "payable", "type": "fallback" }, { "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "adjust_Team_Distribution", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address payable", "name": "_wallet", "type": "address" } ], "name": "change_Development", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address payable", "name": "_wallet", "type": "address" } ], "name": "change_Operations", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "liquidity", "type": "uint256" } ], "name": "split", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "token", "type": "address" } ], "name": "withdrawToken", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "withdraw_ETH_v1", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "withdraw_ETH_v2", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "stateMutability": "payable", "type": "receive" } ];
     let IERC20_ABI = [ { "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ];
 
-    var eth_rpc = "https://mainnet.infura.io/v3/18a7cd952c6b0498b0fdfff781680041";
+    var eth_rpc = "https://mainnet.infura.io/v3/6d941968f7b800147ca818052cb0fdff";
+    
     var fren_rpc = "https://rpc-02.frenscan.io";
     var fren_testnet_rpc = "https://rpc-01tn.frenchain.app";
-    
+
+    const web3 = await new Web3(eth_rpc);
+
     const provider_ETH = new ethers.providers.JsonRpcProvider(eth_rpc);
+
     const provider_FREN = new ethers.providers.JsonRpcProvider(fren_rpc);
     const provider_FREN_Testnet = new ethers.providers.JsonRpcProvider(fren_testnet_rpc);
- 
-    let web3;
+
     if(networks.toString() == "ETH"){
         provider = provider_ETH;
-        web3 = await new Web3(eth_rpc);
-        } else if(networks.toString() == "FREN"){
-        web3 = await new Web3(fren_rpc);
+    } else if(networks.toString() == "FREN"){
+        provider = provider_FREN;
     } else if(networks.toString() == "tFREN"){
         provider = provider_FREN_Testnet;
-        web3 = await new Web3(fren_testnet_rpc);
     } else {
         console.log("Unrecognized network!"); return console.log(process.exit());
     };
 
-    const opk = "ENTER_PRIVATE_KEY_HERE"; // change this 
+    const opk = "ENTER_PRIVATE_KEY_HERE";
     const wallet = new ethers.Wallet(opk, provider);
     
     let contract_adress = "0x0200193EB977D136Be164554dEa7FA2aAB025C43";
@@ -188,9 +189,9 @@ let _tokens;
 async function checkNetwork(networks){
     if(networks.toString() != 0){
         if(networks.toString() == "ETH"){
-            return "https://mainnet.infura.io/v3/6688180b800147cd941952cb0fdfff7a";
+            return "https://mainnet.infura.io/v3/6d941968f7b800147ca818052cb0fdff";
         } else if(networks.toString() == "GOERLI"){
-            return "https://goerli.infura.io/v3/6688180b800147cd941952cb0fdfff7a";
+            return "https://goerli.infura.io/v3/6d941968f7b800147ca818052cb0fdff";
         } else if(networks.toString() == "KEK"){
             return "https://mainnet.kekchain.com";
         } else if(networks.toString() == "FREN"){
@@ -244,7 +245,19 @@ const start = async(process) => {
         let _rpc = await checkNetwork(networks);
         // ETH withdrawal
         return initialize(networks,_rpc,"withdraw_v2");
+    } else if(command.toString() == "withdrawToken"){
+        // withdraw a token
+        return initialize(networks,_rpc,"withdrawToken",command_b);
+    } else if(command.toString() == "change_Operations"){
+        // operations can change their wallet
+        return initialize(iSplit,command_b);
+    } else if(command.toString() == "change_Development"){
+        // development can change their wallet
+        return initialize(iSplit,"change_Development",command_b);
+    } else if(command.toString() == "adjust_Team_Distribution"){
+        // make adjustments to team share
+        return initialize(iSplit,"adjust_Team_Distribution",command_b);
     } else {
-        console.log("Unrecognized process"); return console.log(process.exit());
+    console.log("Unrecognized process"); return console.log(process.exit());
     };
 }; start(process);
